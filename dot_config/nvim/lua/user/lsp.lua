763274ffff6ec85c0eaa5a667262f20894bf831e
Radim Hejduk <dev@radimhejduk.cz>
2024-01-06 09:10:07 +0100

    Update .config/nvim/lua/user/lsp.lua

--- dot_config/nvim/lua/user/lsp.lua
+++ dot_config/nvim/lua/user/lsp.lua
@@ -31,9 +31,15 @@ lspconfig.emmet_language_server.setup{
     on_attach = on_attach,
     capabilities = capabilities,
     filetypes = { "css", "twig", "html", "latte", "javascriptreact", "less", "pug", "sass", "scss", "typescriptreact" },
-    showAbbreviationSuggestions = true,
-    showExpandedAbbreviation = "always",
-    showSuggestionsAsSnippets = true
+    init_options = {
+        showAbbreviationSuggestions = true,
+        showExpandedAbbreviation = "always",
+        showSuggestionsAsSnippets = true,
+        includeLanguages = {
+             twig = "html",
+             latte = "html"
+        }
+    }
 }
 
 
