59670cb03b67950871fd7efe6a0f5f4b164cd0bb
Radim Hejduk <dev@radimhejduk.cz>
2024-01-10 14:24:55 +0100

    Update .config/nvim/lua/user/harpoon.lua

--- dot_config/nvim/lua/user/harpoon.lua
+++ dot_config/nvim/lua/user/harpoon.lua
@@ -5,7 +5,7 @@ harpoon:setup()
 -- REQUIRED
 
 vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
-vim.keymap.set("n", "<C-w>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+vim.keymap.set("n", "<C-w>", function() harpoon.ui:toggle_quick_menu() end)
 
 -- Toggle previous & next buffers stored within Harpoon list
 vim.keymap.set("n", "<C-ú>", function() harpoon:list():prev() end)
