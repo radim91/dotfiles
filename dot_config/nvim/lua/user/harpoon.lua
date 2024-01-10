2dbc79d7453ef6b69e2e9b2ede6ea3a0bc48e0c8
Radim Hejduk <dev@radimhejduk.cz>
2024-01-10 14:57:36 +0100

    Update .config/nvim/lua/user/harpoon.lua

--- dot_config/nvim/lua/user/harpoon.lua
+++ dot_config/nvim/lua/user/harpoon.lua
@@ -12,22 +12,22 @@ vim.keymap.set("n", "<C-ú>", function() harpoon:list():prev() end)
 vim.keymap.set("n", "<C-/>", function() harpoon:list():next() end)
 
 -- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
-
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
-end
-
-vim.keymap.set("n", "<C-w>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
+--[[ local conf = require("telescope.config").values ]]
+--[[ local function toggle_telescope(harpoon_files) ]]
+--[[     local file_paths = {} ]]
+--[[     for _, item in ipairs(harpoon_files.items) do ]]
+--[[         table.insert(file_paths, item.value) ]]
+--[[     end ]]
+--[[]]
+--[[     require("telescope.pickers").new({}, { ]]
+--[[         prompt_title = "Harpoon", ]]
+--[[         finder = require("telescope.finders").new_table({ ]]
+--[[             results = file_paths, ]]
+--[[         }), ]]
+--[[         previewer = conf.file_previewer({}), ]]
+--[[         sorter = conf.generic_sorter({}), ]]
+--[[     }):find() ]]
+--[[ end ]]
+--[[]]
+--[[ vim.keymap.set("n", "<C-w>", function() toggle_telescope(harpoon:list()) end, ]]
+--[[     { desc = "Open harpoon window" }) ]]
