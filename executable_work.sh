08606b2b962bc5a533d9416e02a6c16ccfdff82a
Radim Hejduk <dev@radimhejduk.cz>
2024-01-31 14:35:55 +0100

    Update work.sh

--- executable_work.sh
+++ executable_work.sh
@@ -6,4 +6,3 @@ com.spotify.Client &
 firefox-developer-edition &
 org.mozilla.Thunderbird &
 org.signal.Signal &
-chromium --app="https://mailpo.kr-vysocina.cz" --class=WebApp-IceWarp4803 --name=WebApp-IceWarp4803 --user-data-dir=/home/radim/.local/share/ &
