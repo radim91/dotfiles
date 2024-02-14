6bd5709c72b3d7c67e8952063b4c0b1a810c8317
Radim Hejduk <dev@radimhejduk.cz>
2024-02-14 19:37:21 +0100

    Update .config/nvim/lua/user/plugins.lua

--- dot_config/nvim/lua/user/plugins.lua
+++ dot_config/nvim/lua/user/plugins.lua
@@ -65,6 +65,7 @@ return packer.startup(function(use)
     }
     use "nvim-pack/nvim-spectre";
     use "tpope/vim-fugitive"
+    use "vimpostor/vim-tpipeline"
 
     -- cmp plugins
     use "hrsh7th/nvim-cmp" -- The completion plugin
