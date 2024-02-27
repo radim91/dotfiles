f46374a620c5e5609ea28c62d1faf4fc2b798c5c
Radim Hejduk <dev@radimhejduk.cz>
2024-02-27 15:33:16 +0100

    Add .wezterm.lua

---
+++ dot_wezterm.lua
@@ -0,0 +1,21 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+config.window_decorations = "INTEGRATED_BUTTONS|RESIZE"
+config.font = wezterm.font 'Inconsolata Nerd Font'
+config.warn_about_missing_glyphs = false
+config.font_size = 13.5
+
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
+}
+
+-- and finally, return the configuration to wezterm
+return config
+
