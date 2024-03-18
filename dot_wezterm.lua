896c4a20afce14776f810ab37d5ce240eb1e619b
Radim Hejduk <dev@radimhejduk.cz>
2024-03-18 13:54:31 +0100

    Update .wezterm.lua

--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -53,7 +53,24 @@ config.keys = {
     key = 'a',
     mods = 'LEADER|CTRL',
     action = wezterm.action.SendKey { key = 'a', mods = 'CTRL' },
-  },
+  }
+}
+
+
+config.mouse_bindings = {
+	{
+		event = { Down = { streak = 1, button = "Right" } },
+		mods = "NONE",
+		action = wezterm.action_callback(function(window, pane)
+			local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+			if has_selection then
+				window:perform_action(act.CopyTo("ClipboardAndPrimarySelection"), pane)
+				window:perform_action(act.ClearSelection, pane)
+			else
+				window:perform_action(act({ PasteFrom = "Clipboard" }), pane)
+			end
+		end),
+	},
 }
 
 config.window_padding = {
