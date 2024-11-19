2035df57f0fc39ac7e8aa46fbc06dd7c3fe2c2fb
Radim Hejduk <dev@radimhejduk.cz>
2024-11-19 14:28:18 +0100

    Update .config/nvim/lua/core/keymaps.lua Update .config/nvim/lua/core/plugins.lua

--- dot_config/nvim/lua/core/plugins.lua
+++ dot_config/nvim/lua/core/plugins.lua
@@ -62,12 +62,7 @@ local plugins = {
     'sontungexpt/sttusline', dependecies = { 'nvim-tree/nvim-web-devicons' },
     'folke/persistence.nvim', event = "BufReadPre",
     'folke/noice.nvim', event = "VeryLazy", dependencies = { 'MunifTanjim/nui.nvim', 'rcarriga/nvim-notify' },
-    'linux-cultist/venv-selector.nvim',
-        dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
-        keys = {
-            { '<leader>vs', '<cmd>VenvSelect<cr>' },
-            { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
-        },
+    'linux-cultist/venv-selector.nvim', dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' }
 }
 
 local opts = {}
