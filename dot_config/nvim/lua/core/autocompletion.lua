49d221257d00abdeb87214372966b5cba4e3bdd6
Radim Hejduk <dev@radimhejduk.cz>
2025-02-03 15:51:58 +0100

    Update .config/nvim/lua/core/autocompletion.lua

--- dot_config/nvim/lua/core/autocompletion.lua
+++ dot_config/nvim/lua/core/autocompletion.lua
@@ -27,10 +27,22 @@ cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = 
 
 require("nvim-surround").setup()
 require("codecompanion").setup({
+    adapters = {
+        deepseekr1 = function()
+          return require("codecompanion.adapters").extend("ollama", {
+            name = "deepseek", -- Give this adapter a different name to differentiate it from the default ollama adapter
+            schema = {
+              model = {
+                default = "deepseek-r1:latest",
+              },
+            },
+          })
+        end,
+    },
     strategies = { -- Change the adapters as required
-        chat = { adapter = "ollama" },
-        inline = { adapter = "ollama" },
-        agent = { adapter = "ollama" },
+        chat = { adapter = "deepseekr1" },
+        inline = { adapter = "deepseekr1" },
+        agent = { adapter = "deepseekr1" },
     },
     log_level = "TRACE",
 })
