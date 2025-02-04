41653a4f59c910810d81d950d191cb8a3a33fe58
Radim Hejduk <dev@radimhejduk.cz>
2025-02-04 08:18:23 +0100

    Update work.sh

--- executable_work.sh
+++ executable_work.sh
@@ -2,7 +2,7 @@
 
 ghostty &
 gtk-launch webapp-Discord9665.desktop &
-gtk-launch webapp-Spotify0685.desktop &
+dev.alextren.Spot &
 firefox-developer-edition &
 org.mozilla.Thunderbird &
 org.signal.Signal &
