31a5f544a6d9b7d160d61478617d8770761b044a
Radim Hejduk <dev@radimhejduk.cz>
2025-07-05 10:57:04 +0200

    Update .config/nvim/lua/core/lsp.lua

--- dot_config/nvim/lua/core/lsp.lua
+++ dot_config/nvim/lua/core/lsp.lua
@@ -15,55 +15,55 @@ require("mason-lspconfig").setup({
     }
 })
 
-lspconfig.phpactor.setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-    init_options = {
-        ["language_server_phpstan.enabled"] = true,
-        ["language_server_psalm.enabled"] = false,
-    }
-}
-
-lspconfig.ts_ls.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
-
-lspconfig.gopls.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
-
-lspconfig.rust_analyzer.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
-
-lspconfig.emmet_language_server.setup{
-    on_attach = on_attach,
-    capabilities = capabilities,
-    filetypes = { "css", "html.twig", "twig", "html", "latte", "javascriptreact", "less", "pug", "sass", "scss", "typescriptreact" },
-    init_options = {
-        showAbbreviationSuggestions = true,
-        showExpandedAbbreviation = "always",
-        showSuggestionsAsSnippets = true,
-    }
-}
-
-lspconfig.pyright.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
-
-lspconfig.clangd.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
-
-lspconfig.sqls.setup{
-    on_attach = on_attach,
-    capabilities = capabilities
-}
+--[[ lspconfig.phpactor.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities, ]]
+--[[     init_options = { ]]
+--[[         ["language_server_phpstan.enabled"] = true, ]]
+--[[         ["language_server_psalm.enabled"] = false, ]]
+--[[     } ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.ts_ls.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.gopls.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.rust_analyzer.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.emmet_language_server.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities, ]]
+--[[     filetypes = { "css", "html.twig", "twig", "html", "latte", "javascriptreact", "less", "pug", "sass", "scss", "typescriptreact" }, ]]
+--[[     init_options = { ]]
+--[[         showAbbreviationSuggestions = true, ]]
+--[[         showExpandedAbbreviation = "always", ]]
+--[[         showSuggestionsAsSnippets = true, ]]
+--[[     } ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.pyright.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.clangd.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
+--[[]]
+--[[ lspconfig.sqls.setup{ ]]
+--[[     on_attach = on_attach, ]]
+--[[     capabilities = capabilities ]]
+--[[ } ]]
 
 local luasnip = require 'luasnip'
 local cmp = require 'cmp'
